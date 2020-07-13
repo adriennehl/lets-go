5b45f57bbdeb0451b849f3c4bd79e328ddb49f0d
Adrienne Li <ahli@fb.com>
2020-07-13 12:07:19 -0500

    Initial Commit

---
+++ TripPlanner/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  TripPlanner
+//
+//  Created by Adrienne Li on 7/13/20.
+//  Copyright © 2020 ahli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ TripPlanner/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TripPlanner
+//
+//  Created by Adrienne Li on 7/13/20.
+//  Copyright © 2020 ahli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
