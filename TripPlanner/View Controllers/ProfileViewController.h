e053c750acec0c443485789d70f11b31c7eae15c
Adrienne Li <ahli@fb.com>
2020-07-13 16:13:15 -0500

    enable user log out

---
+++ TripPlanner/View Controllers/ProfileViewController.h
@@ -0,0 +1,17 @@
+//
+//  ProfileViewController.h
+//  TripPlanner
+//
+//  Created by Adrienne Li on 7/13/20.
+//  Copyright © 2020 ahli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ProfileViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
