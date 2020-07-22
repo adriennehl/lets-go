93a7dfee8835532ff564f43fbe9bda39de4ddae6
Adrienne Li <ahli@fb.com>
2020-07-22 10:53:22 -0500

    user can see a list of past trips on their profile page and click on a trip to see details

--- TripPlanner/View Controllers/ProfileViewController.m
+++ TripPlanner/View Controllers/ProfileViewController.m
@@ -9,10 +9,12 @@
 #import "ProfileViewController.h"
 #import "LoginViewController.h"
 #import "SceneDelegate.h"
+#import "TripCell.h"
 #import <Parse/Parse.h>
 
-@interface ProfileViewController ()
-
+@interface ProfileViewController () <UITableViewDelegate, UITableViewDataSource>
+@property (weak, nonatomic) IBOutlet UITableView *pastTripsTableView;
+@property (nonatomic, strong) NSArray *trips;
 @end
 
 @implementation ProfileViewController
@@ -20,6 +22,12 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    self.pastTripsTableView.delegate = self;
+    self.pastTripsTableView.dataSource = self;
+}
+
+- (void)viewDidAppear:(BOOL)animated {
+    [self fetchPastTrips];
 }
 
 // enable user log out
@@ -36,14 +44,58 @@
     }];
 }
 
-/*
+// get past trips from user's trips relation
+- (void)fetchPastTrips {
+    PFRelation *relation = [PFUser.currentUser relationForKey:@"trips"];
+    PFQuery *query = [relation query];
+    [query orderByDescending:@"startDate"];
+    [query whereKey:@"endDate" lessThan:[NSDate date]];
+    
+    [query findObjectsInBackgroundWithBlock:^(NSArray *trips, NSError *error) {
+      if (!error) {
+          self.trips = trips;
+          [self.pastTripsTableView reloadData];
+      } else {
+          NSLog(@"Error: %@", error.localizedDescription);
+      }
+    }];
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return self.trips.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    TripCell *cell = [tableView dequeueReusableCellWithIdentifier:@"TripCell" forIndexPath:indexPath];
+    cell = [cell setCell:self.trips[indexPath.row]];
+    return cell;
+}
+
+// set height based on the photo aspect ratio
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    Trip *trip = self.trips[indexPath.row];
+    if (trip.aspectRatio != 0 ){
+        return 150 * trip.aspectRatio + 130;
+    }
+    return 150 * 1.5 +70;
+}
+
 #pragma mark - Navigation
 
 // In a storyboard-based application, you will often want to do a little preparation before navigation
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
     // Get the new view controller using [segue destinationViewController].
     // Pass the selected object to the new view controller.
+    
+    if([segue.identifier isEqualToString:@"detailSegue"]){
+           // get destination view controller
+           TripViewController *destinationViewController = [segue destinationViewController];
+           TripCell *tappedCell = sender;
+           // get indexPath of tapped cell
+           NSIndexPath *indexPath = [self.pastTripsTableView indexPathForCell:tappedCell];
+           // get trip of tapped cell
+           destinationViewController.trip = self.trips[indexPath.row];
+       }
 }
-*/
 
 @end
